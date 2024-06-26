7437c9a13705bd8d09cd45d85b6711d72c5ea4c1
Jonathan Mendoza <john.2114@hotmail.com>
2024-06-26 15:00:19 +0200

    Add project files.

---
+++ LinqToSql/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+using System.Configuration;
+namespace LinqToSql
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            LinqToSqlDataClassesDataContext dataContext;
+            InitializeComponent();
+
+            // string connectionString = ConfigurationManager.ConnectionString["LinqTo.Sql.Properties.Settings.JonathanDBConnectionString"].ConnectionString;
+            string connectionString = ConfigurationManager.ConnectionStrings["LinqToSql.Properties.Settings.JonathanDBConnectionString"].ConnectionString;
+
+
+            dataContext = new LinqToSqlDataClassesDataContext(connectionString);
+        }
+    }
+}
