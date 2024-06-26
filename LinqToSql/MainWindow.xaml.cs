28832051d977a6e532708c6ce4f98db1322a31d8
Jonathan Mendoza <john.2114@hotmail.com>
2024-06-26 17:28:06 +0200

    Proyect : SQL with Linq : UPDATED 1/3

--- LinqToSql/MainWindow.xaml.cs
+++ LinqToSql/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 
 
 using System.Configuration;
+using LinqToSql.JonathanDBDataSetTableAdapters;
 namespace LinqToSql
 {
     /// <summary>
@@ -22,16 +23,102 @@ namespace LinqToSql
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Connection Class
+        LinqToSqlDataClassesDataContext dataContext;
         public MainWindow()
         {
-            LinqToSqlDataClassesDataContext dataContext;
             InitializeComponent();
 
-            // string connectionString = ConfigurationManager.ConnectionString["LinqTo.Sql.Properties.Settings.JonathanDBConnectionString"].ConnectionString;
+            //Code to Connect our Linq with Sql Server.. Important!!
             string connectionString = ConfigurationManager.ConnectionStrings["LinqToSql.Properties.Settings.JonathanDBConnectionString"].ConnectionString;
-
-
             dataContext = new LinqToSqlDataClassesDataContext(connectionString);
+
+            // Calling insertUniversities Method
+
+            //insertUniversities();
+            InsertStudents();
         }
+
+
+
+        public void insertUniversities()
+        {
+            //Cleaning or deleting duplicate data
+            dataContext.ExecuteCommand("delete from University");
+
+            //Add new data to the University table
+            University yale = new University(); 
+            yale.Name = "Yale";
+            dataContext.Universities.InsertOnSubmit(yale);
+
+            
+            //Second University
+            University HSAugsburg = new University();
+            HSAugsburg.Name = ("Ausburg University");
+            dataContext.Universities.InsertOnSubmit(HSAugsburg);
+
+            //SumitChanges
+            dataContext.SubmitChanges();
+
+            //Display the new data of the University to the table
+            MainDataGrid.ItemsSource = dataContext.Universities;
+          
+
+            
+
+
+
+        }
+
+        public void InsertStudents()
+        {
+
+            University yale = dataContext.Universities.First(un => un.Name.Equals("Yale"));
+            University HSAugsburg = dataContext.Universities.First(un => un.Name.Equals("Ausburg University"));
+
+
+            List<Student> students = new List<Student>();
+
+            // initialization of a new student
+            students.Add(new Student { 
+              
+              Name = "Carla",
+              Gender = "female", 
+              UniversityId = yale.Id 
+            });
+
+            students.Add(new Student { 
+              
+              Name = "Tony",
+              Gender = "Male", 
+              University = HSAugsburg 
+            });
+
+            students.Add(new Student { 
+              
+              Name = "Leyla",
+              Gender = "female", 
+              University = yale 
+            });
+
+            students.Add(new Student { 
+              
+              Name = "Lixx",
+              Gender = "Transgender", 
+              University = HSAugsburg
+            });
+
+            dataContext.Students.InsertAllOnSubmit(students);
+            //SumitChanges
+            dataContext.SubmitChanges();
+
+            //Display the new data of the University to the table
+            MainDataGrid.ItemsSource = dataContext.Students;
+
+        }
+
+
+
+
     }
 }
