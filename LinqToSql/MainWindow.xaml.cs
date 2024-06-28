61e274b99508f483c6f26d8407af1aa1848128ea
Jonathan Mendoza <john.2114@hotmail.com>
2024-06-28 14:11:19 +0200

    Update and Delete Code Added. Project DEMO Complete.

--- LinqToSql/MainWindow.xaml.cs
+++ LinqToSql/MainWindow.xaml.cs
@@ -36,7 +36,21 @@ namespace LinqToSql
             // Calling insertUniversities Method
 
             //insertUniversities();
-            InsertStudents();
+            //InsertStudents();
+
+            //insertLectures();
+            //InsertLectureAssociations();
+            //GetUniversityOfTony();
+
+            // GetLecturesFromToni();
+
+            //getAllStudentFromAux();
+            //GetAllUniversityWithTransgenders();
+
+            //GetAllLectureFromYale();    
+            // UpdateToni();
+
+            DeleteLixx();                
         }
 
 
@@ -118,7 +132,129 @@ namespace LinqToSql
         }
 
 
+        public void insertLectures()
+        {
 
+            dataContext.Lectures.InsertOnSubmit( new Lecture { Name = "Math"});
+            dataContext.Lectures.InsertOnSubmit( new Lecture { Name = "History"});
+
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.Lectures;
+
+        }
+
+        public void InsertLectureAssociations()
+        {
+
+            Student carla = dataContext.Students.First(st => st.Name.Equals("Carla"));
+            Student tony = dataContext.Students.First(st => st.Name.Equals("tony"));
+            Student leyle = dataContext.Students.First(st => st.Name.Equals("Leyla"));
+            Student jame = dataContext.Students.First(st => st.Name.Equals("Lixx"));
+
+            Lecture Math = dataContext.Lectures.First(lc => lc.Name.Equals("Math"));
+            Lecture history = dataContext.Lectures.First(lc => lc.Name.Equals("History"));
+
+
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = carla, Lecture = Math });
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = tony, Lecture = Math });
+
+            StudentLecture slToni = new StudentLecture();
+            slToni.Student = tony;
+            slToni.LectureId = history.Id;
+            dataContext.StudentLectures.InsertOnSubmit(slToni);
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = leyle, Lecture = history });
+
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.StudentLectures;
+
+        }
+
+
+        public void GetUniversityOfTony()
+        {
+            Student tony = dataContext.Students.First(st => st.Name.Equals("Tony"));
+            University tonysuniversity = tony.University;
+
+            List<University> uni = new List<University>();
+            uni.Add(tonysuniversity);
+            MainDataGrid.ItemsSource = uni;
+
+
+        }
+
+        public void GetLecturesFromToni()
+        {
+
+            Student tony = dataContext.Students.First(st => st.Name.Equals("Tony"));
+
+            var tonisLecture = from s1 in tony.StudentLectures select s1.Lecture;
+
+            MainDataGrid.ItemsSource = tonisLecture;
+
+        }
+
+
+         public void getAllStudentFromAux()
+        {
+            var stundetFromAux = from student in dataContext.Students
+                                 where student.University.Name == "Ausburg University"
+                                 select student;
+
+            MainDataGrid.ItemsSource = stundetFromAux;
+        }
+
+        public void GetAllUniversityWithTransgenders()
+        {
+
+            var transgenderPeople = from student in dataContext.Students
+                                    join university in dataContext.Universities
+                                    on student.University equals university
+                                    where student.Gender == "Transgender"
+                                    select university;
+
+            MainDataGrid.ItemsSource = transgenderPeople;
+        }
+
+
+        public void GetAllLectureFromYale()
+
+            //Posible Bug oder Incomplete Datam : the Transgender Stundent currently isnt enrolled to a Lecture
+        {
+            var lecturesFromYale = from sl in dataContext.StudentLectures
+                                   join student in dataContext.Students
+                                   on sl.StudentId equals student.Id
+                                   where student.University.Name == "Yale"
+                                   select sl.Lecture;
+            MainDataGrid.ItemsSource = lecturesFromYale;
+        }
+
+
+        //Update InformationData
+        public void UpdateToni()
+        {
+
+            Student Toni = dataContext.Students.FirstOrDefault(st => st.Name == "tony");
+            Toni.Name = "Antonio";
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
+
+
+        }
+
+        //Delete InformationData
+
+        public void DeleteLixx()
+        {
+            Student Lixx = dataContext.Students.FirstOrDefault(st => st.Name == "Lixx");
+
+            dataContext.Students.DeleteOnSubmit(Lixx);
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
+        }
 
     }
 }
